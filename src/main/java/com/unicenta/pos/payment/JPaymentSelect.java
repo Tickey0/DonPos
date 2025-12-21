02250d3a4faef7e072bae63bf721875d91077e3e
Jorge Luis <jorgeluis@resolvedor.dev>
2025-12-21 13:15:09 -0500

    [IMP] Set ESC key for close the dialog window

--- src/main/java/com/unicenta/pos/payment/JPaymentSelect.java
+++ src/main/java/com/unicenta/pos/payment/JPaymentSelect.java
@@ -38,13 +38,17 @@ import dev.joguenco.pos.establishment.DataLogicEstablishment;
 import dev.joguenco.pos.establishment.EstablishmentInfo;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
 import javax.swing.SwingWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.WordUtils;
@@ -109,7 +113,7 @@ public abstract class JPaymentSelect extends javax.swing.JDialog
         initComponents();
         this.applyComponentOrientation(o);
         getRootPane().setDefaultButton(m_jButtonOK);
-
+        setEscKeyForClose();
     }
 
     /**
@@ -1612,6 +1616,24 @@ public abstract class JPaymentSelect extends javax.swing.JDialog
         } catch (BasicException ex) {
             txtAddress.setText("No definida");
         }
+    }
+
+    /**
+     * Set ESC key for close the dialog window
+     */
+    final void setEscKeyForClose() {
+        ActionListener escListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        };
+
+        getRootPane().registerKeyboardAction(
+                escListener,
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW
+        );
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
