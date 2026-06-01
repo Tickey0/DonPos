f8098d5def20e87107a54f8f14f360072fc891a6
Jorge Luis <jorgeluis@resolvedor.dev>
2026-05-31 20:50:39 -0500

    [IMP] Upgrade purchase report, set the date range

--- src/main/java/com/unicenta/beans/DateUtils.java
+++ src/main/java/com/unicenta/beans/DateUtils.java
@@ -16,9 +16,10 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with uniCenta oPOS.  If not, see <http://www.gnu.org/licenses/>.
-
 package com.unicenta.beans;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -28,56 +29,58 @@ import java.util.GregorianCalendar;
  * @author JG uniCenta
  */
 public class DateUtils {
-    
-    /** Creates a new instance of DateUtils */
+
+    /**
+     * Creates a new instance of DateUtils
+     */
     private DateUtils() {
     }
-    
+
     /**
      *
      * @return
      */
-    public static Date getToday() { 
+    public static Date getToday() {
         // el dia de hoy sin horas ni nada.        
-        return getToday(new Date());     
+        return getToday(new Date());
     }
-    
+
     /**
      *
      * @param d
      * @return
      */
-    public static Date getToday(Date d) { 
+    public static Date getToday(Date d) {
         // el dia de hoy sin horas ni nada.        
         GregorianCalendar ddate = new GregorianCalendar();
-        ddate.setTime(d);    
+        ddate.setTime(d);
         GregorianCalendar ddateday = new GregorianCalendar(ddate.get(GregorianCalendar.YEAR), ddate.get(GregorianCalendar.MONTH), ddate.get(GregorianCalendar.DAY_OF_MONTH));
-        return ddateday.getTime();        
+        return ddateday.getTime();
     }
-    
+
     /**
      *
      * @return
      */
-    public static Date getTodayMinutes() { 
+    public static Date getTodayMinutes() {
         // el dia de hoy sin horas ni nada.        
-        return getTodayMinutes(new Date());     
+        return getTodayMinutes(new Date());
     }
-    
+
     /**
      *
      * @param d
      * @return
      */
-    public static Date getTodayMinutes(Date d) { 
+    public static Date getTodayMinutes(Date d) {
         // el dia de hoy sin horas ni nada.        
         GregorianCalendar ddate = new GregorianCalendar();
-        ddate.setTime(d);    
-        GregorianCalendar ddateday = new GregorianCalendar(ddate.get(GregorianCalendar.YEAR), ddate.get(GregorianCalendar.MONTH), ddate.get(GregorianCalendar.DAY_OF_MONTH)
-                                                         , ddate.get(GregorianCalendar.HOUR_OF_DAY), ddate.get(GregorianCalendar.MINUTE));
-        return ddateday.getTime();        
+        ddate.setTime(d);
+        GregorianCalendar ddateday = new GregorianCalendar(ddate.get(GregorianCalendar.YEAR), ddate.get(GregorianCalendar.MONTH), ddate.get(GregorianCalendar.DAY_OF_MONTH),
+                 ddate.get(GregorianCalendar.HOUR_OF_DAY), ddate.get(GregorianCalendar.MINUTE));
+        return ddateday.getTime();
     }
-    
+
     /**
      *
      * @param d
@@ -86,16 +89,16 @@ public class DateUtils {
     public static Date getTodayHours(Date d) {
         // el dia ajustado a las horeas     
         Calendar ddate = Calendar.getInstance();
-        ddate.setTime(d);    
-        
+        ddate.setTime(d);
+
         Calendar dNow = Calendar.getInstance();
         dNow.clear();
-        dNow.set(ddate.get(Calendar.YEAR), ddate.get(Calendar.MONTH), ddate.get(Calendar.DAY_OF_MONTH)
-               , ddate.get(Calendar.HOUR_OF_DAY), 0, 0);
-        
-        return dNow.getTime();        
+        dNow.set(ddate.get(Calendar.YEAR), ddate.get(Calendar.MONTH), ddate.get(Calendar.DAY_OF_MONTH),
+                 ddate.get(Calendar.HOUR_OF_DAY), 0, 0);
+
+        return dNow.getTime();
     }
-    
+
     /**
      *
      * @param day
@@ -103,19 +106,25 @@ public class DateUtils {
      * @return
      */
     public static Date getDate(Date day, Date hour) {
-        
+
         // Calculamos el dia actual con la hora escogida.
         Calendar dDay = Calendar.getInstance();
         dDay.setTime(day);
         Calendar dHour = Calendar.getInstance();
-        dHour.setTime(hour);    
-        
+        dHour.setTime(hour);
+
         Calendar dNow = Calendar.getInstance();
         dNow.clear();
-        dNow.set(dDay.get(Calendar.YEAR), dDay.get(Calendar.MONTH), dDay.get(Calendar.DAY_OF_MONTH)
-        , dHour.get(Calendar.HOUR_OF_DAY), dHour.get(Calendar.MINUTE), dHour.get(Calendar.SECOND));
-        
+        dNow.set(dDay.get(Calendar.YEAR), dDay.get(Calendar.MONTH), dDay.get(Calendar.DAY_OF_MONTH),
+                 dHour.get(Calendar.HOUR_OF_DAY), dHour.get(Calendar.MINUTE), dHour.get(Calendar.SECOND));
+
         return dNow.getTime();
     }
 
+    public static Date getFirstDayOfMonth() {
+        LocalDate today = LocalDate.now();
+        LocalDate firstDay = today.withDayOfMonth(1);
+
+        return Date.from(firstDay.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
 }
